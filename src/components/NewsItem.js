fa3372f6fbb1d41eed1a1a0e51fa70eb828232da
Dima <address>
2021-02-20 13:45:34 +0300

    X

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -10,7 +10,7 @@ const NewsItem = ({el}) => {
             <div className="news-item__date">{el.date}</div>
             <h2>{el.title}</h2>
             <p>{el.desc}</p>
-            <Link to={`/news/${el.id}`} className="btn btn-primary">Читать далее</Link>
+            <Link to={`/single-page-app/news/${el.id}`} className="btn btn-primary">Читать далее</Link>
         </div>
     )
 }
